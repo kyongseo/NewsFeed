7c97ce302d52e8ac95ee7a2295343dc3b9552168
kyongseo <pokj930@naver.com>
2024-06-18 14:21:43 +0900

    commit

---
+++ src/main/java/hello/blog/entity/Gender.java
@@ -0,0 +1,5 @@
+package hello.blog.entity;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/hello/blog/entity/User.java
@@ -0,0 +1,24 @@
+package hello.blog.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+//@Entity
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private Gender gender;
+    private String phone;
+    private String address;
+    private String nicName;
+    private int profileImage;
+
+
+}
