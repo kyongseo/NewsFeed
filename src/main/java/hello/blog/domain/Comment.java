e32982f4fb7809c8bbccdc92b359c204c0d7e500
kyongseo <pokj930@naver.com>
2024-06-24 16:46:49 +0900

    commit

--- src/main/java/hello/blog/domain/Comment.java
+++ src/main/java/hello/blog/domain/Comment.java
@@ -1,4 +1,30 @@
 package hello.blog.domain;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+//@Entity
+//@Table(name = "comments")
+//@Getter @Setter
 public class Comment {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "post_id", nullable = false)
+//    private Post post;
+//
+//    @Column(nullable = false)
+//    private String content;
+//
+//    @CreatedDate
+//    @Column(name = "created_at", updatable = false)
+//    private LocalDateTime createdA = LocalDateTime.now();
 }
