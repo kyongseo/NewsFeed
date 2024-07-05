ee50836df6513f6d4fe4699dc51a4012021fa003
kyongseo <pokj930@naver.com>
2024-07-05 11:09:05 +0900

    add Admin by security

--- src/main/java/hello/blog/controller/AdminController.java
+++ src/main/java/hello/blog/controller/AdminController.java
@@ -1,5 +1,6 @@
 package hello.blog.controller;
 
+import hello.blog.domain.Post;
 import hello.blog.service.PostService;
 import hello.blog.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -10,40 +11,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Set;
 
-//@Controller
-//@RequiredArgsConstructor
+
+@Controller
+@RequiredArgsConstructor
 public class AdminController {
 
-//    private final UserService userService;
-//    private final PostService postService;
-//
-//
-//    @GetMapping("/admin")
-//    public String adminPage(Model model) {
-//        model.addAttribute("users", postService.getAllPosts());
-//        return "admin/adminpage";
-//    }
-//
-//    @GetMapping("/admin/dashboard")
-//    public String showAdminDashboard(Model model) {
-//        model.addAttribute("users", userService.findAllUsers());
-//        model.addAttribute("posts", postService.getAllPosts());
-//        return "admin/dashboard";
-//    }
+    private final UserService userService;
+    private final PostService postService;
 
-    // 관리자 대시보드 -- 시큐리티 배으고 나중에 구현..
-//    @GetMapping("/admin/dashboard")
-//    public String showAdminDashboard(Model model) {
-//        model.addAttribute("username", "admin");
-//        return "redirect:/";
-//    }
 
-//    @GetMapping("/@{username}/posts")
-//    public String getUserPosts(@PathVariable("username") String username, Model model) {
-//        Set<Post> userPosts = userService.getUserPosts(username);
-//        model.addAttribute("username", username);
-//        model.addAttribute("posts", userPosts);
-//        return "blog";
-//    }
+    @GetMapping("/admin")
+    public String adminPage(Model model) {
+        model.addAttribute("users", postService.getAllPosts());
+        return "admin/adminpage";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String showAdminDashboard(Model model) {
+        model.addAttribute("users", userService.findAllUsers());
+        model.addAttribute("posts", postService.getAllPosts());
+        return "admin/dashboard";
+    }
+
+
+
+    @GetMapping("/@{username}/posts")
+    public String getUserPosts(@PathVariable("username") String username, Model model) {
+        Set<Post> userPosts = userService.getUserPosts(username);
+        model.addAttribute("username", username);
+        model.addAttribute("posts", userPosts);
+        return "blog";
+    }
 }
