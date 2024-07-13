138ad819fc6a4bce6594c5e18a87783b3599dcab
kyongseo <pokj930@naver.com>
2024-07-13 17:42:47 +0900

    Feat: 좋아요 기능

--- src/main/java/hello/blog/controller/LikeController.java
+++ src/main/java/hello/blog/controller/LikeController.java
@@ -4,6 +4,7 @@ import hello.blog.domain.Like;
 import hello.blog.domain.Post;
 import hello.blog.domain.User;
 import hello.blog.repository.LikeRepository;
+import hello.blog.service.LikeService;
 import hello.blog.service.PostService;
 import hello.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +19,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LikeController {
 
-    private final LikeRepository likeRepository;
+    private final LikeService likeService;
     private final PostService postService;
     private final UserService userService;
 
     // 좋아요 버튼을 눌렀을 때
     @PostMapping("/posts/{postId}/like")
-    public String like(@PathVariable("postId") Long postId,
-                       Authentication authentication) {
-
+    public String like(@PathVariable("postId") Long postId, Authentication authentication) {
         if (authentication != null && authentication.isAuthenticated()) {
             String username = authentication.getName();
             Optional<User> userOptional = userService.findByUserName(username);
@@ -35,24 +34,10 @@ public class LikeController {
                 User user = userOptional.get();
                 Optional<Post> postOptional = postService.getPostById(postId);
 
-                if (postOptional.isPresent()) {
-                    Post post = postOptional.get();
-                    Optional<Like> existingLike = likeRepository.findByPostAndUser(post, user);
-
-                    if (existingLike.isPresent()) {
-                        // 이미 좋아요가 있는 경우, 좋아요 취소
-                        likeRepository.delete(existingLike.get());
-                    } else {
-                        // 좋아요 추가
-                        Like like = new Like();
-                        like.setPost(post);
-                        like.setUser(user);
-                        likeRepository.save(like);
-                    }
-                }
+                postOptional.ifPresent(post -> likeService.like(post, user));
             }
         }
-        // 게시글 상세 페이지로 리다이렉트
+
         return "redirect:/posts/" + postId;
     }
 }
