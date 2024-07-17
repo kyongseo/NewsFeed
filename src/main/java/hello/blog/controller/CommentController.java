96711cc60151d998a514ddd2e5b63ac231c85323
kyongseo <pokj930@naver.com>
2024-07-17 20:47:09 +0900

    Feat: 댓글 기능 추가

--- src/main/java/hello/blog/controller/CommentController.java
+++ src/main/java/hello/blog/controller/CommentController.java
@@ -12,12 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final PostService postService;
     private final CommentService commentService;
 
     @PostMapping("/post/{postId}/comments")
     public String addComment(@PathVariable("postId") Long postId,
-                             @RequestParam String content,
+                             @RequestParam("content") String content,
                              Authentication authentication,
                              Model model) {
         if (authentication != null && authentication.isAuthenticated()) {
@@ -27,19 +26,6 @@ public class CommentController {
             return "redirect:/loginform";
         }
     }
-
-//    @PostMapping("/post/{postId}/comments")
-//    public String addComment(@PathVariable("postId") Long postId,
-//                             @RequestParam String content,
-//                             Authentication authentication) {
-//        if (authentication != null && authentication.isAuthenticated()) {
-//            commentService.addComment(postId, content);
-//            return "redirect:/posts/" + postId;
-//        } else {
-//            return "redirect:/loginform";
-//        }
-//    }
-
 
     // 댓글 수ㅠ정
     @PostMapping("/comment/{commentId}/update")
@@ -60,20 +46,4 @@ public class CommentController {
         // 삭제 후 해당 포스트 페이지로 리다이렉트
         return "redirect:/posts/" + postId;
     }
-
-//    // 대댓글 작성
-//    @PostMapping("/comment/{parentCommentId}/reply")
-//    public String addReply(@PathVariable("parentCommentId") Long parentCommentId,
-//                           @RequestParam String content,
-//                           Authentication authentication,
-//                           RedirectAttributes attributes) {
-//        if (authentication != null && authentication.isAuthenticated()) {
-//            commentService.addReply(parentCommentId, content);
-//            Long postId = commentService.getPostIdByCommentId(parentCommentId);
-//            attributes.addAttribute("postId", postId); // 포스트 ID를 리다이렉트 파라미터로 추가
-//            return "redirect:/posts/{postId}";
-//        } else {
-//            return "redirect:/loginform";
-//        }
-//    }
 }
