b3bd4166faa12f8a97812ef1a86ad3e6377fb044
kyongseo <pokj930@naver.com>
2024-08-25 20:54:23 +0900

    feat: refreshToken 관리

--- src/main/java/hello/blog/feature/repository/RefreshTokenRepository.java
+++ src/main/java/hello/blog/feature/repository/RefreshTokenRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
     Optional<RefreshToken> findByValue(String value);
+
     boolean existsByValue(String token);
 
     Optional<RefreshToken> findByUserId(Long userId);
